9faa0b2c439def4a87de14b6d590e9f1779c2ec7
Aditya T. <adit.thakur26@gmail.com>
2024-10-26 23:47:48 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const productRoutes = require('./routes/productRoutes'); // Ensure the path is correct
+const authRoutes = require('./routes/authRoutes');
+
+require('dotenv').config();
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', productRoutes); // Make sure '/api' is the correct base path for your API
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
