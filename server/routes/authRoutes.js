4cd4357b14050501ca97da7df513fdf8916316ef
Aditya T. <adit.thakur26@gmail.com>
2024-10-26 23:47:24 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,9 +10,10 @@ router.post('/login', async (req, res) => {
     const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
     const user = result.rows[0];
     if (!user || !(await bcrypt.compare(password, user.password))) {
+        console.log(password, user.password);
       return res.status(400).send('Invalid credentials');
     }
-    const token = jwt.sign({ id: user.id, isAdmin: user.is_admin }, 'your_jwt_secret', { expiresIn: '1h' });
+    const token = jwt.sign({ id: user.id, isAdmin: user.is_admin }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
   } catch (error) {
     res.status(500).send('Server error');
