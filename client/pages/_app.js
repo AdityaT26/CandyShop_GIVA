b97cbc79bbdfbf855c3337d66c18c8e142198153
Aditya T. <adit.thakur26@gmail.com>
2024-10-26 23:45:16 +0530

    Update _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,30 +1,17 @@
-import { useEffect } from 'react';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css';
-import { ThemeProvider, useTheme } from 'next-themes';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
-      <MainApp Component={Component} pageProps={pageProps} />
+      <div className="bg-primary text-highlight min-h-screen transition-colors duration-300">
+        <NavBar />
+        <main className="p-4">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </ThemeProvider>
-  );
-}
-
-function MainApp({ Component, pageProps }) {
-  const { theme, setTheme } = useTheme();
-
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-
-  return (
-    <div className="bg-primary text-highlight min-h-screen transition-colors duration-300">
-      <NavBar toggleTheme={toggleTheme} isDarkMode={theme === 'dark'} />
-      <main className="p-4">
-        <Component {...pageProps} />
-      </main>
-    </div>
   );
 }
 
