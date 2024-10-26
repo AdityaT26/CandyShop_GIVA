342fdf30b81b34f5205cfd84b27644cd9a1f676d
Aditya T. <adit.thakur26@gmail.com>
2024-10-26 23:47:09 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,23 +1,20 @@
 // authMiddleware.js
 const jwt = require('jsonwebtoken');
 
-const authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization')?.split(' ')[1];
-  if (!token) return res.status(401).send('Access denied');
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) return res.sendStatus(401); // Unauthorized
 
-  try {
-    const decoded = jwt.verify(token, 'your_jwt_secret');
-    req.user = decoded;
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403); // Forbidden
+    req.user = user; // Attach user info to request
     next();
-  } catch (error) {
-    res.status(400).send('Invalid token');
-  }
-};
+  });
+}
 
-const authorizeAdmin = (req, res, next) => {
-  if (!req.user.isAdmin) return res.status(403).send('Access forbidden');
+function authorizeAdmin(req, res, next) {
+  if (req.user.role !== 'admin') return res.sendStatus(403); // Forbidden
   next();
-};
+}
 
-module.exports.authenticateToken = authenticateToken; // Export individually
-module.exports.authorizeAdmin = authorizeAdmin;
+module.exports = { authenticateToken, authorizeAdmin };
