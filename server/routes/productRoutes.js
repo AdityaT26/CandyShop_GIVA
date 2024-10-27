6e25d0c01548b843d8913b190366af3cbc2cda87
Aditya T. <adit.thakur26@gmail.com>
2024-10-27 22:45:48 +0530

    Update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -7,6 +7,5 @@ router.get('/products', productController.getProducts);
 router.post('/products', productController.addProduct); // Allow adding products without authentication
 router.put('/products/:id', productController.updateProduct); // Allow updating products without authentication
 router.delete('/products/:id', productController.deleteProduct); // Allow deleting products without authentication
-router.get('/admin/statistics', productController.getStatistics);
 
 module.exports = router;
